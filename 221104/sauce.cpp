d00ce1a687e8db1984af9b8efe4c03eed1570074
윤건희 <116573141+Nuguri9@users.noreply.github.com>
2022-11-08 20:57:54 +0900

    Create sauce.cpp

---
+++ 221104/sauce.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "1head.h"
+#include "2head.h"
+
+using namespace std;
+Sound TWol;
+
+int main() {
+	TWol.TvVol(50);	
+	Num KBS;
+	Num MBC;
+	Num SBS;
+	Num JTBC = jtbc;
+	print << "TV의 소리는:" << TWol.TvVol(50) << endl;
+		print << "TV의 채널은: " << JTBC << endl;
+}
